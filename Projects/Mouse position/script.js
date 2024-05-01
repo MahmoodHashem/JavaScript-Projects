d52aaa506ec7c24d5f7d85cfde190c48d2c2a619
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-05-01 22:20:46 +0430

    Complete mouse position project

--- Projects/Mouse position/script.js
+++ Projects/Mouse position/script.js
@@ -1,2 +1,20 @@
 
 
+window.onload = init; 
+
+
+function init(){
+    let map = document; 
+map.onmousemove = showCoords; 
+}
+
+function showCoords(eventObj){
+    let x = document.getElementById('x-h2');  
+    let y = document.getElementById('y-h2');  
+    let xcoord = eventObj.clientX;  
+    let ycoord = eventObj.clientY; 
+
+        x.textContent = xcoord; 
+        y.textContent = ycoord; 
+
+}
