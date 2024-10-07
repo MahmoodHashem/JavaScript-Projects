fb693ae9818f12b1376e484c3eed95b2fa14b1e4
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-10-07 23:40:00 +0430

    feat(keyboard): Develop keyboard visualizer app
    - Implemented **Interactive Buttons** that change appearance when clicked to enhance user experience
    - Added **Copy to Clipboard** functionality for easy text copying from the input field
    - Designed with **Responsive Design** to ensure optimal usability on both desktop and mobile devices

---
+++ Projects/keyboard-visualizer/Readme.md
@@ -0,0 +1,28 @@
+# Keyboard Visualization
+
+A dynamic virtual keyboard application that allows users to type in multiple languages (English, Persian, Pashto) using a graphical interface. The app features a customizable keyboard layout, clipboard functionality, and an interactive user experience.
+
+## Screenshot
+
+![desktop](./screenshots/desktop.png)
+![mobile](./screenshots/mobile.png)
+
+## Links
+
+- [Live Demo](https://mahmoodhashem.github.io/JavaScript-Projects/Projects/keyboard-visualizer/index.html)
+
+## Features
+
+- **Interactive Buttons**: Buttons change appearance when clicked, enhancing user experience.
+- **Multi-language Support**: Switch between English, Persian, and Pashto keyboard layouts.
+- **Dynamic Keyboard Rendering**: The keyboard layout updates based on user selection.
+- **Copy to Clipboard**: Easily copy text from the input field to the clipboard.
+- **Backspace Functionality**: Remove the last character typed with a dedicated backspace button.
+- **Responsive Design**: The app is designed to work well on both desktop and mobile devices.
+
+## What I Learned
+
+- The use of JavaScript event listeners for **mouse and keyboard events.**
+- Implementing the **Clipboard API** for copying text and handling cross-browser compatibility.
+- **Structuring data** in JavaScript for multi-language support..
+- The importance of error handling and user feedback in web applications.
---
+++ Projects/keyboard-visualizer/script.js
@@ -0,0 +1,179 @@
+const keyboardElement = document.getElementById("keyboard");
+const selected = document.querySelector("#langs");
+const textInput = document.getElementById("textInput");
+
+const keyboardLangs = {
+    english: {
+        numbers: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "0",
+            "Backspace",
+        ],
+        top: ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        middle: ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+        bottom: ["Z", "X", "C", "V", "B", "N", "M"],
+    },
+
+    persian: {
+        numbers: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "0",
+            "Backspace",
+        ],
+        top: ["ض", "ص", "ث", "ق", "ف", "غ", "ع", "ه", "خ", "ح", "ج", "چ"],
+        middle: ["ش", "س", "ی", "ب", "ل", "ا", "ت", "ن", "م", "ک", "گ", "پ"],
+        bottom: ["ظ", "ط", "ز", "ر", "ذ", "د", "ئ", "و"],
+    },
+    pashto: {
+        numbers: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "0",
+            "Backspace",
+        ],
+        top: ["ض", "ص", "ث", "ق", "ف", "غ", "ع", "ه", "ج", "چ", "ځ", "خ"],
+        middle: ["ښ", "ش", "س", "ۍ", "ی", "ي", "ب", "ل", "ا", "ت", "ټ", "پ"],
+        bottom: ["ر", "ذ", "ظ", "ط", "د", "ډ", "ړ", "ئ", "ې", "و", "ړ", "ږ"],
+        bottom2: ["ڼ", "ن", "م", "ک", "ګ", "ز", "ژ", "ء", "ح"],
+    },
+};
+
+function render(language) {
+
+    keyboardElement.innerHTML = "";
+
+
+    for (const row in language) {
+        const rowDiv = document.createElement("div");
+        rowDiv.className = row;
+
+        language[row].forEach((key) => {
+            const keyButton = document.createElement("button");
+            keyButton.className = "key";
+            keyButton.textContent = key;
+            if (keyButton.textContent === "Backspace") {
+                keyButton.classList.add("backspace");
+            }
+            keyButton.addEventListener("mousedown", () => {
+                keyButton.classList.add("active");
+                // Add the clicked key to the input field
+                if (key === " ") {
+                    textInput.value += " ";
+                } else if (key === "Backspace") {
+                    textInput.value = textInput.value.slice(0, -1);
+                } else {
+                    textInput.value += key;
+                }
+            });
+            keyButton.addEventListener("mouseup", () => {
+                keyButton.classList.remove("active");
+            });
+            rowDiv.appendChild(keyButton);
+        });
+
+        keyboardElement.appendChild(rowDiv);
+    }
+
+
+    const spaceButton = document.createElement("button");
+    spaceButton.className = "key space";
+    spaceButton.textContent = "Space";
+    spaceButton.addEventListener("mousedown", () => {
+        spaceButton.classList.add("active");
+        textInput.value += " ";
+    });
+    spaceButton.addEventListener("mouseup", () => {
+        spaceButton.classList.remove("active");
+    });
+    keyboardElement.appendChild(spaceButton);
+
+}
+
+
+render(keyboardLangs.english);
+
+selected.addEventListener("change", () => {
+    const lang = selected.value;
+    if (lang === "fa") {
+        render(keyboardLangs.persian);
+    } else if (lang === "pashto") {
+        render(keyboardLangs.pashto);
+    } else {
+        render(keyboardLangs.english);
+    }
+});
+
+document.addEventListener("keydown", (event) => {
+    const key = event.key.toUpperCase();
+    const keyElements = document.querySelectorAll(".key");
+    keyElements.forEach((k) => {
+        if (
+            k.textContent === key ||
+            (key === " " && k.classList.contains("space"))
+        ) {
+            k.classList.add("active");
+        }
+    });
+});
+
+document.addEventListener("keyup", (event) => {
+    const key = event.key.toUpperCase();
+    const keyElements = document.querySelectorAll(".key");
+    keyElements.forEach((k) => {
+        if (
+            k.textContent === key ||
+            (key === " " && k.classList.contains("space"))
+        ) {
+            k.classList.remove("active");
+        }
+    });
+});
+
+document.getElementById("copy").addEventListener("click", function () {
+    const inputField = textInput;
+    const textToCopy = inputField.value;
+
+    if (navigator.clipboard) {
+        navigator.clipboard
+            .writeText(textToCopy)
+            .then(() => {
+                alert("Copied");
+            })
+            .catch((err) => {
+                alert("Failed to copy text: " + err);
+            });
+    } else {
+
+        inputField.select();
+        try {
+            const successful = document.execCommand("copy");
+            const msg = successful ? "Copied" : "Failed to copy";
+            alert(msg);
+        } catch (err) {
+            alert("Failed to copy text: " + err);
+        }
+    }
+});
