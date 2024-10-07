fd5d94faa96587f56b347884cb11ff7f88e908c1
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-10-07 16:21:53 +0430

    refactor(bmi): Enhance BMI calculator with new features and accessibility
    - Refactored JavaScript code for improved readability and functionality
    - Added accessibility features to button and input fields for better usability
    - Implemented additional features to enhance the BMI calculator's functionality
    - Improved overall user experience with a more intuitive interface and interactions

--- Projects/bmi/script.js
+++ Projects/bmi/script.js
@@ -1,64 +1,79 @@
+const inputs = document.querySelectorAll('input');
+const form = document.querySelector('form');
+const resultBox = document.querySelector('.result-container');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let isValid = true;
 
-const error = document.querySelectorAll('.error');
-
-function calculate() {
-
-    const height = document.querySelector('#height');
-    const weight = document.querySelector('#weight');
-
-    const resultNum = document.querySelector(".num"); 
-    const result = document.querySelector('.result'); 
-
-    height.style.border = 'none'
-        weight.style.border = 'none';
-        error[0].style.display = 'none';
-        error[1].style.display = 'none';
-
-    if (height.value === '' && weight.value === '') {
-        height.style.border = '2px solid red'
-        weight.style.border = '2px solid red';
-        error[0].style.display = 'block';
-        error[1].style.display = 'block';
-    } else if(height.value === '' || weight.value === '' ) {
-        if (height.value === '') {
-            error[0].style.display = 'block';
-            height.style.border = '2px solid red'
-        } else{
-            error[1].style.display = 'block';
-            weight.style.border = '2px solid red';
+    inputs.forEach(input => {
+        const errorMessage = validateInput(input);
+        if (errorMessage) {
+            input.parentElement.dataset.error = errorMessage;
+            isValid = false; 
+        } else {
+            input.parentElement.dataset.error = "";
         }
-    } else {
-            const bmi = calculateBMI(height.value, weight.value); 
-            resultNum.textContent = bmi
-            const r = bmiState(bmi); 
-            result.textContent = r; 
-        }
+    });
+
+    if (isValid) {
+        displayResult();
+    }
+});
+
+function validateInput(input) {
+    if (input.validity.valueMissing) {
+        return input.id === 'height' 
+            ? "Please, enter your height" 
+            : "Please, enter your weight";
+    }
+    
+    if (input.validity.rangeOverflow || input.validity.rangeUnderflow) {
+        return input.id === "height" 
+            ? "Please enter a valid height" 
+            : "Please enter a valid weight";
+    }
+
+    return ""; // Input is valid
+}
+
+function displayResult() {
+    const bmiElement = resultBox.querySelector('.num');
+    const stateElement = resultBox.querySelector('.result');
+    const height = inputs[0].value;
+    const weight = inputs[1].value;
+
+    const result = calculateBMI(height, weight);
+    
+    bmiElement.innerHTML = result; 
+    stateElement.innerHTML = bmiState(result);
 }
 
 
-function calculateBMI(height, weight){
-    height =  height / 100;  
-    let bmi = (weight / (height * height)).toFixed(2); 
 
-    return bmi; 
+function calculateBMI(height, weight) {
+    height = height / 100;
+    let bmi = (weight / (height * height)).toFixed(2);
+
+    return bmi;
 }
 
-function bmiState(bmi){
-    if(bmi < 0){
+function bmiState(bmi) {
+    if (bmi < 0) {
         return 'Invalid BMI';
     };
-    if(bmi < 18){
-        return 'UnderWeight'; 
+    if (bmi < 18) {
+        return 'UnderWeight';
     };
-    if(bmi < 25){
+    if (bmi < 25) {
         return 'Normal';
     };
-    if( bmi < 30){
+    if (bmi < 30) {
         return 'Overweight';
     };
 
-        return "Obese";
+    return "Obese";
 }
+
 
 
